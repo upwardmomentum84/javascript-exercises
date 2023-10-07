abcbb311e29a047efdac227ca00a63c19f17daf8
upwardmomentum84 <brandonsteed@automattic.com>
2023-10-06 21:48:13 -0400

    finished 09_palidromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function ( word ) {
+
+    word = word.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,""); // strip punctuation
+    word = word.toLowerCase();
+
+    var forward = word.split("");
+    var reversed = word.split("");
+    reversed = reversed.reverse();
+
+
+
+    //console.log(forward);
+    //console.log(reversed);
+
+    for (i = 0; i < forward.length; i++ ) {
+        if ( forward[i] != reversed[i]  )
+            return false;
+    }
+
+   return true;
 
 };
 
