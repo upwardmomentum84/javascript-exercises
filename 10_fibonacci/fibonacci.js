0d0742fc04881c9d32d3ee767cade4a85eded7a4
upwardmomentum84 <brandonsteed@automattic.com>
2023-10-07 22:17:09 -0400

    completed exercise 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function( whichOne ) {
+    var runningTotal = 1;
+    var previousTotal = 0;
+    var previousValue = 0;
 
+    if ( Number(whichOne < 0) )
+        return "OOPS";
+
+    for ( var i = 1; i < Number(whichOne); i++ ) {
+        previousValue = runningTotal;
+        runningTotal += previousTotal;
+        previousTotal = previousValue;
+ 
+        console.log(previousTotal + " " + runningTotal);
+
+    }
+    return runningTotal;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function( people ) {
 
+    const oldest = people.sort(function(a, b) {
+
+        if ( !a.yearOfDeath ){
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if ( !b.yearOfDeath ){
+           b.yearOfDeath = new Date().getFullYear();
+        }
+
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson < nextPerson ? -1 : 1;
+      });
+
+    return oldest.pop();
 };
 
 // Do not edit below this line
